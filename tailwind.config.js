50df48bce8c35b3f342de020a5333fba23a31da5
Thomas <thomasvdeursen@gmail.com>
2021-07-27 19:28:41 +0200

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['index.html'],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
