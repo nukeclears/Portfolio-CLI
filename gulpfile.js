04906f958dae54b70e48dcff2afce5f03517f2ca
Thomas <thomasvdeursen@gmail.com>
2021-07-27 19:44:08 +0200

    Browsersync functional

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// gulpfile.js
+const { watch, series, src, dest } = require("gulp");
+var browserSync = require("browser-sync").create();
+var postcss = require("gulp-postcss");
+const imagemin = require("gulp-imagemin");
+
+// Task for compiling our CSS files using PostCSS
+function cssTask(cb) {
+    return src("./src/styles/*.css") // read .css files from ./src/styles folder
+        .pipe(postcss()) // compile using postcss
+        .pipe(dest("./assets/css")) // paste them in ./assets/css folder
+        .pipe(browserSync.stream());
+    cb();
+}
+
+// Task for minifying images
+function imageminTask(cb) {
+    return src("./assets/images/*")
+        .pipe(imagemin())
+        .pipe(dest("./assets/images"));
+    cb();
+}
+
+// Serve from browserSync server
+function browsersyncServe(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        },
+    });
+    cb();
+}
+
+function browsersyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+// Watch Files & Reload browser after tasks
+function watchTask() {
+    watch("./**/*.html", series(cssTask, browsersyncReload));
+    watch(["./src/styles/*.css"], series(cssTask, browsersyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(cssTask, browsersyncServe, watchTask);
+exports.css = cssTask;
+exports.images = imageminTask;
